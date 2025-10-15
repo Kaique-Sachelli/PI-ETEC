06d8bac032911036905dd862c7d3840143c4e953
Gustavo <ghmsilva21@gmail.com>
2025-10-15 17:03:45 -0300

    inserção das informções do banco

--- bancodedados.js
+++ bancodedados.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 
 // aqui como no siga viagem voce coloca as coisas do sqwl
 const pool = mysql.createPool({
-  host: ' ',     // endereço
-  user: ' ',          //usuário
-  password: ' ',          //senha
-  database: ' '   //nome do banco 
+  host: 'localhost',     // endereço
+  user: 'root',          //usuário
+  password: '',          //senha
+  database: 'agendamento_lab'   //nome do banco 
 });
 
 module.exports = pool;
