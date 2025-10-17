e73cc3a58bf84498a871063bc2274a0a7d1bee07
PedroMoreno <peetemoreno@gmail.com>
2025-10-16 22:58:41 -0300

    adicionando funcionalidade

--- produtos.js
+++ produtos.js
@@ -6,3 +6,41 @@ function adicionarAoKit(elemento) {
     kitSelecionado.push({ nome: nomeProduto, imagem: imagemProduto });
     atualizarKit();
 }
+
+function removerDoKit(index) {
+    kitSelecionado.splice(index, 1);
+    atualizarKit();
+}
+
+function atualizarKit() {
+    const kitContainer = document.querySelector(".kit-container");
+    let listaContainer = kitContainer.querySelector(".kit-lista");
+    if (!listaContainer) {
+        listaContainer = document.createElement("div");
+        listaContainer.classList.add("kit-lista");
+        kitContainer.insertBefore(listaContainer, kitContainer.querySelector(".finalize-button"));
+    }
+    listaContainer.innerHTML = "";
+    kitSelecionado.forEach((item, index) => {
+        const div = document.createElement("div");
+        div.classList.add("kit-item");
+        div.innerHTML = `
+            <img src="${item.imagem}" alt="${item.nome}" class="kit-img">
+            <span class="kit-nome">${item.nome}</span>
+            <button class="remover-item">&times;</button>
+        `;
+
+
+        div.querySelector(".remover-item").addEventListener("click", () => removerDoKit(index));
+        listaContainer.appendChild(div);
+    });
+    const addIcon = document.querySelector(".add-icon");
+    if (kitSelecionado.length > 0) {
+        addIcon.style.display = "none";
+    } else {
+        addIcon.style.display = "block";
+    }
+}
+
+
+
