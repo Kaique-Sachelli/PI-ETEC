598ecd3d67c84dc78b056018e8cb4e7bafe2096c
Kaique Sachelli <kaique.safefe@gmail.com>
2025-11-05 12:48:54 -0300

    Arrumando js e html do login

--- JS/login.js
+++ JS/login.js
@@ -1,101 +1,65 @@
 // Código do login
 import { mostrarNotificao } from "./notificacao.js";
 
-document.getElementById('botaoLogin').addEventListener('click', async function (e) {
-    e.preventDefault();
+let campoEmail;
+let campoSenha;
+let idUsuario;
+let toggleSenha;
 
-    const email = document.getElementById('emailLogin').value;
-    const senha = document.getElementById('senhaLogin').value;
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('botaoLogin').addEventListener('click', async function (e) {
+        e.preventDefault();
 
-    if (email && senha) {
-        try {
-            const resposta = await fetch('http://localhost:3000/login', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ email, senha })
-            });
-            const dados = await resposta.json();
-            campoEmail = document.getElementById('emailLogin')
-            campoSenha = document.getElementById('senhaLogin')
-            if (dados.sucesso) {
-                mostrarNotificao(dados.mensagem,'sucesso')
-                idUsuario = dados.idUsuario
-                module.senha = idUsuario
-                setTimeout(() => {
-                    window.location.href = '../HTML/inicio.html';
-                }, 1500);
+        const email = document.getElementById('emailLogin').value;
+        const senha = document.getElementById('senhaLogin').value;
+
+        if (email && senha) {
+            try {
+                const resposta = await fetch('http://localhost:3000/login', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ email, senha })
+                });
+                const dados = await resposta.json();
+                campoEmail = document.getElementById('emailLogin')
+                campoSenha = document.getElementById('senhaLogin')
+                if (dados.sucesso) {
+                    mostrarNotificao(dados.mensagem,'sucesso')
+                    idUsuario = dados.idUsuario
+                    sessionStorage.setItem('idUsuario', String(idUsuario))
+                    setTimeout(() => {
+                        window.location.href = '../HTML/inicio.html';
+                    }, 1500);
+                }
+                else{
+                    mostrarNotificao(dados.mensagem, 'erro')
+                        document.getElementById('emailLogin').value = ''
+                        document.getElementById('senhaLogin').value = ''
+
+                }
+            } catch (erro) {
+                console.error('Erro ao tentar fazer login:', erro);
             }
-            else{
-                mostrarNotificao(dados.mensagem, 'erro')
-                    document.getElementById('emailLogin').value = ''
-                    document.getElementById('senhaLogin').value = ''
-
-            }
-        } catch (erro) {
-            console.error('Erro ao tentar fazer login:', erro);
+        } else {
+            mostrarNotificao('Preencha todos os campos!', 'erro')
         }
-    } else {
-        mostrarNotificao('Preencha todos os campos!', 'erro')
-    }
 
-    // let tipoUsuarioLogado;
-
-    // Simulação de verificação (substitua pela lógica real)
-    // if (username === 'professor' && password === 'senha') {
-    //     tipoUsuarioLogado = 'professor';
-    // } else if (username === 'tecnico' && password === 'senha') {
-    //     tipoUsuarioLogado = 'tecnico';
-    // } else if (username === 'administrador' && password === 'senha') {
-    //     tipoUsuarioLogado = 'adm';
-    // } else {
-    //     alert('Usuário ou senha inválidos');
-    //     return;
-    // }
-
-    // Chama a função para verificar o tipo de usuário
-    // verificarTipoUsuario(tipoUsuarioLogado);
+        
 });
 
-// Código de Mostrar Senha / Ocultar Senha
-function toggleSenha() {
-    const input = document.getElementById("senhaLogin");
-    const icon = document.querySelector(".toggle-senha");
+    // Event listener do toggle senha
+    document.querySelector('.toggle-senha').addEventListener('click', function() {
+        const input = document.getElementById("senhaLogin");
+        const icon = document.querySelector(".toggle-senha");
 
-    if (input.type === "password") {
-        input.type = "text";
-        icon.classList.remove("fa-eye-slash");
-        icon.classList.add("fa-eye");
-    } else {
-        input.type = "password";
-        icon.classList.remove("fa-eye");
-        icon.classList.add("fa-eye-slash");
-    }
-}
-
-
-// Função para mostrar a seção correta com base no tipo de usuário
-
-// function verificarTipoUsuario(tipoUsuario) {
-
-    // Esconder todas as seções inicialmente
-    
-    // document.querySelectorAll('.professor, .tecnico, .adm').forEach(section => {
-    //     section.style.display = 'none';
-    // });
-
-    // Mostrar a seção correspondente ao tipo de usuário
-
-//     if (tipoUsuario === 'professor') {
-//         document.querySelectorAll('.professor').forEach(section => {
-//             section.style.display = 'block';
-//         });
-//     } else if (tipoUsuario === 'tecnico') {
-//         document.querySelectorAll('.tecnico').forEach(section => {
-//             section.style.display = 'block';
-//         });
-//     } else if (tipoUsuario === 'adm') {
-//         document.querySelectorAll('.adm').forEach(section => {
-//             section.style.display = 'block';
-//         });
-//     }
-// }
+        if (input.type === "password") {
+            input.type = "text";
+            icon.classList.remove("fa-eye-slash");
+            icon.classList.add("fa-eye");
+        } else {
+            input.type = "password";
+            icon.classList.remove("fa-eye");
+            icon.classList.add("fa-eye-slash");
+        }
+    });
+});
