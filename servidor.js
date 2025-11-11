ae0f3c5f4cfd24fd4106db636193c0c65ca577e2
GUSTAVO HENRIQUE MARTIN SILVA <25.00855-8@maua.br>
2025-11-11 11:01:38 -0300

    Adiciona buscar kits

--- servidor.js
+++ servidor.js
@@ -66,12 +66,12 @@ app.post("/login", async (req, res) => {
           sucesso: true,
           mensagem: "Login realizado com sucesso!",
           token: token,
-        });        
+        });
       } else {
-        res.json({ sucesso: false, mensagem: "Email ou senha incorretos." });        
+        res.json({ sucesso: false, mensagem: "Email ou senha incorretos." });
       }
     } else {
-        res.json({ sucesso: false, mensagem: "Email ou senha incorretos." });        
+      res.json({ sucesso: false, mensagem: "Email ou senha incorretos." });
     }
   } catch (erro) {
     res.status(500).json({ erro: erro.message });
@@ -177,23 +177,23 @@ app.post("/usuarios/atualizar", verificarToken, async (req, res) => {
     return res.status(403).json({
       sucesso: false
     })
-   }else {
+  } else {
     try {
       const hashSenha = await bcrypt.hash(novaSenha, saltrounds) //Não salva mais senhas em texto puro
-       const [result] = await pool.query(
-         'UPDATE usuario SET email = ?, senha = ?, permissao = ? WHERE idUsuario = ?',
-         [email, hashSenha, permissao, idUsuario]
-       );
-       res.json({
-         sucesso: true,
-         mensagem: 'Dados atualizados com sucesso!'
-       });
-     } catch (erro) {
-       res.json({
-         sucesso: false,
-         mensagem: 'Erro ao atualizar: ' + erro.message
-       })
-     }
+      const [result] = await pool.query(
+        'UPDATE usuario SET email = ?, senha = ?, permissao = ? WHERE idUsuario = ?',
+        [email, hashSenha, permissao, idUsuario]
+      );
+      res.json({
+        sucesso: true,
+        mensagem: 'Dados atualizados com sucesso!'
+      });
+    } catch (erro) {
+      res.json({
+        sucesso: false,
+        mensagem: 'Erro ao atualizar: ' + erro.message
+      })
+    }
   }
 });
 
@@ -457,9 +457,33 @@ app.post('/kits/salvar', verificarToken, async (req, res) => {
       mensagem: 'Houve um erro ao salvar o kit',
       erro: error.message
     })
-  }finally{
-    if(connection) connection.release(); //depois de toda operação libera a conexão
+  } finally {
+    if (connection) connection.release(); //depois de toda operação libera a conexão
   }
 })
+//buscar kits
+app.get('/kits/buscar', verificarToken, async (req, res) => {
+  const idUSuario = req.usuario.idUsuario;
+  try {
+    const [rows] = await pool.query(
+      `SELECT k.idKit, k.idUsuario, k.nome, k.descrição,
+      FROM kits k,
+      INNER JOIN usuario u,
+      WHERE k.idUsuario = u.idUsuario AND k.idUsuario = ?;`,
+      [idUSuario]
+      );
+
+res.json({
+  sucesso: true,
+  usuarios: rows
+});
+
+    } catch (erro) {
+  res.status(500).json({
+    sucesso: false,
+    mensagem: 'Erro ao buscar Kits: ' + erro.message
+  });
+}
+});
 const PORTA = 3000;
 app.listen(PORTA, () => console.log(`🚀 Servidor rodando na porta ${PORTA}`));
